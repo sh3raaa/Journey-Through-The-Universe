3dd0bf7abc90fa0b8b4506b1b25dd16d8ae2d2d0
sh3raaa <usefullhamoudi@gmail.com>
2026-09-23 23:10:58 -0400

    Delete cct360 lab1 directory

--- cct360 lab1/js/script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", function() {
-    var scrollPosition = window.scrollY;
-    var stars = document.querySelector(".stars");
-    stars.style.transform = "translateY(" + scrollPosition * 0.15 + "px)";
-});
